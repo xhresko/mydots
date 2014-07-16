935c69a704db6896adef7cd4576edf0d3e913228
Juraj Hresko <juraj@hresko-juraj-wifi.kancelar.seznam.cz>
2014-07-16 10:21:38 +0200

    Initial commit.

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Init config files from .mydots directory.
+#
+
+rm -fr .bash_profile ~/.bashrc ~/.gitattributes ~/.gitignore ~/.gitconfig ~/.vim ~/.vimrc ~/.gvimrc
+
+ln -s ~/.mydots/shell/bash_profile ~/.bash_profile
+ln -s ~/.mydots/shell/bashrc ~/.bashrc
+ln -s ~/.mydots/git/gitattributes ~/.gitattributes
+ln -s ~/.mydots/git/gitignore ~/.gitignore
+ln -s ~/.mydots/git/gitconfig ~/.gitconfig
+ln -s ~/.mydots/vim/ ~/.vim
+ln -s ~/.mydots/vim/vimrc ~/.vimrc
+ln -s ~/.mydots/vim/gvimrc ~/.gvimrc
