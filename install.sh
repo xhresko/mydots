6585b32b6a0a4cebb7dcc2961a06a227e1798dd3
Juraj Hreško <juraj.hresko@firma.seznam.cz>
2014-08-11 15:19:00 +0200

    New i3 settings
    - i3bar - removed unused parts
    - notifications with dunst

--- install.sh
+++ install.sh
@@ -15,7 +15,8 @@ ln -sf ~/.mydots/vim/vimrc ~/.vimrc
 ln -sf ~/.mydots/vim/gvimrc ~/.gvimrc
 ln -sf ~/.mydots/i3/ ~/.i3
 ln -sf ~/.mydots/X/Xdefaults ~/.Xdefaults
+ln -sf ~/.mydots/i3/i3status.conf ~/.i3status.conf
 
 # git clone git://github.com/klen/python-mode.git ~/.mydots/vim/bundle/python-mode
-git clone git://github.com/altercation/vim-colors-solarized.git ~/.mydots/vim/bundle/vim-colors-solarized
-git clone git://github.com/tpope/vim-fugitive.git ~/.mydots/vim/bundle/vim-fugitive
+git clone  -q git://github.com/altercation/vim-colors-solarized.git ~/.mydots/vim/bundle/vim-colors-solarized
+git clone -q git://github.com/tpope/vim-fugitive.git ~/.mydots/vim/bundle/vim-fugitive
