ac1308eaddcf00f0c53ace3b0e1130f8e7e7abd0
Juraj Hreško <juraj.hresko@gmail.com>
2021-06-02 20:07:00 -0400

    Improve startup of Emacs

--- emacs/init.el
+++ emacs/init.el
@@ -4,23 +4,6 @@
 (load-theme 'wombat)
 ;; Maximize window
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
-;; Open TODO
-(find-file "~/Documents/org/todo.org") 
-(switch-to-buffer "todo.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen nil)
- '(initial-buffer-choice "~/Documents/org/todo.org")
- '(package-selected-packages '(markdown-mode)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
 ;; Set default font
 (set-face-attribute 'default nil
@@ -36,4 +19,19 @@
 (package-initialize)
 
 ;; Show line numbers
- (setq line-number-mode t)
+(setq line-number-mode t)
+(when (version<= "26.0.50" emacs-version)
+  (global-display-line-numbers-mode))
+
+;; Remember commands
+(savehist-mode 1)
+
+;; Add Rust binaries to path
+(setq exec-path (append exec-path '("~/.cargo/bin")))
+
+;; Reopen stuff from last session
+(desktop-save-mode 1)
+
+;; Open TODO
+(find-file "~/Documents/org/todo.org") 
+(switch-to-buffer "todo.org")
